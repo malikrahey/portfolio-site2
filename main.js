f86f453b9de027e512150ab3cfa3f34dab92b942
Admin <mjrahey@mun.ca>
2021-08-31 21:01:43 -0230

    first commit (transfer)

---
+++ main.js
@@ -0,0 +1,81 @@
+import './style.css'
+import * as THREE from 'three';
+const scene = new THREE.Scene();
+
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight,0.1,1000);
+
+//const bg = new THREE.TextureLoader().load('blue.jpg');
+//scene.background = bg;
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg'),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth,window.innerHeight);
+//renderer.setClearColor(new THREE.Color('#21282a'),1);
+renderer.render(scene,camera);
+
+const geometry = new THREE.TorusGeometry(10,3,16,100);
+
+const material = new THREE.PointsMaterial({size: 0.005});
+const torus = new THREE.Points(geometry,material);
+torus.position.z = -30;
+
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(15,5,-5);
+
+
+const particlesMaterial = new THREE.PointsMaterial({
+  size: 0.005,
+
+});
+const particlesGeometry = new THREE.BufferGeometry;
+const particlesCount = 50000;
+
+const posArray = new Float32Array(particlesCount * 3);
+
+for(let i = 0; i < particlesCount*3; i++) {
+  posArray[i] = 5000*(Math.random() - 0.5);
+}
+
+particlesGeometry.setAttribute('position', new THREE.BufferAttribute(posArray,3));
+
+
+const particlesMesh = new THREE.Points(particlesGeometry,material);
+
+scene.add(pointLight);
+scene.add(torus, particlesMesh);
+
+
+const cameraOffset = 50;
+const cameraBeginPos = camera.position.z;
+
+
+function moveCamera() {
+  const t = document.body.getBoundingClientRect().top;
+
+
+  camera.position.z = t * -0.01;
+  camera.position.x = t * 0.01;
+
+}
+
+document.body.onscroll = moveCamera;
+const clock = new THREE.Clock();
+function animate() {
+
+
+  const elapsedTime = clock.getElapsedTime();
+
+  particlesMesh.rotation.x = 0.05*elapsedTime;
+  //torus.rotation.x += 0.01;
+  torus.rotation.y += 0.005;
+
+
+  requestAnimationFrame(animate);
+  renderer.render(scene,camera);
+}
+
+animate()
---
+++ projects.js
@@ -0,0 +1,70 @@
+import * as THREE from 'three';
+import './style.css';
+
+//Initialization
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight,0.1,1000);
+camera.position.set(0,0,2)
+const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector('#bg'),
+  });
+
+
+const loader = new THREE.TextureLoader();
+const texture = loader.load('texture.jpg');
+const height = loader.load('clouds.jpg');
+const alpha = loader.load('alpha.png');
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth,window.innerHeight);
+//renderer.setClearColor(new THREE.Color('#21282a'),1);
+renderer.render(scene,camera);
+
+//Light
+const pointLight = new THREE.PointLight('#00b3ff');
+pointLight.position.set(2,3,4);
+
+//objects
+
+const geometry = new THREE.PlaneGeometry(3,3,64,64);
+
+//materials
+
+const material = new THREE.MeshStandardMaterial({
+    color:'gray',
+    map: texture,
+    displacementMap: height,
+    displacementScale: 0.7,
+    alphaMap: alpha
+});
+
+const plane = new THREE.Mesh(geometry,material);
+plane.rotation.x = 181
+plane.position.z = -1
+scene.add(pointLight,plane)
+
+const cameraOffset = 50;
+const cameraBeginPos = camera.position.z;
+
+
+function moveCamera() {
+  const t = document.body.getBoundingClientRect().top;
+
+
+  camera.position.z = 2 + t * -0.001;
+
+}
+
+document.body.onscroll = moveCamera;
+const clock = new THREE.Clock();
+function animate() {
+
+
+  const elapsedTime = clock.getElapsedTime();
+
+  plane.rotation.z += 0.00075;
+  requestAnimationFrame(animate);
+  renderer.render(scene,camera);
+}
+
+animate()
